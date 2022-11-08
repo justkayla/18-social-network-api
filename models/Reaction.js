7cc0a33f2b6f2d5b552778d593aa0c210c426807
Kayla Justice <kaylarjustice@gmail.com>
2022-11-07 18:33:12 -0600

    Building Reaction model

---
+++ models/Reaction.js
@@ -0,0 +1,26 @@
+const { Schema, Types } = require("mongoose");
+
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    // Use a getter method to format the timestamp on query
+  },
+});
+
+module.exports = reactionSchema;
