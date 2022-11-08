a27ed2c164dfa888f7a3074ec9dfb5f73a021366
Kayla Justice <kaylarjustice@gmail.com>
2022-11-07 18:27:51 -0600

    Building Thought model

--- models/User.js
+++ models/User.js
@@ -1,34 +1,48 @@
 const { Schema, model } = require("mongoose");
-const thoughtSchema = require('./Thought');
+const thoughtSchema = require("./Thought");
 
 const validateEmail = function (email) {
   const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   return regex.test(email);
 };
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email address is required"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate: [validateEmail, "Please provide a valid email address"],
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please provide a valid email address",
+      ],
+    },
+    thoughts: [thoughtSchema],
+    friends: [userSchema],
   },
-  email: {
-    type: String,
-    required: [true, "Email address is required"],
-    unique: true,
-    trim: true,
-    lowercase: true,
-    validate: [validateEmail, "Please provide a valid email address"],
-    match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      "Please provide a valid email address",
-    ],
-  },
-  thoughts: [thoughtSchema],
-  friends: [userSchema],
-});
+  // Verify if this is correct virtuals syntax
+  {
+    toJSON: {
+      virtuals: {
+        friendCount: {
+          get() {
+            return this.friends.length;
+          },
+        },
+      },
+    },
+  }
+);
 
-const User = model('user', userSchema);
+const User = model("user", userSchema);
 
 module.exports = User;
